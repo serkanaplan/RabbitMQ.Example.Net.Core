88b64b8ff791fa7ebb56acd18757cf2565ea8751
serkanaplan <serkanaplan@gmail.com>
2024-06-27 17:31:35 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    // Uri = new Uri("amqps://fkucsvcj:LqAqt0v5ApdauHz4EbhyT-mizaXEKoEI@cow.rmq2.cloudamqp.com/fkucsvcj")
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();//Bu metod, RabbitMQ sunucusuna bir bağlantı oluşturur.
+using var channel = connection.CreateModel();//Bu metod, mesaj göndermek veya almak için kullanılacak bir kanal oluşturur.
+
+// queue: Kuyruğun adı. Bu örnekte "hello-queue".
+// durable: Kuyruğun kalıcı olup olmadığını belirtir.
+    // true: Kuyruk kalıcı olur, yani RabbitMQ sunucusu yeniden başlatıldığında bile varlığını korur.
+    // false: Kuyruk geçici olur, yani RabbitMQ sunucusu yeniden başlatıldığında kaybolur.
+// exclusive: Kuyruğun özel olup olmadığını belirtir.
+//     true: Kuyruk sadece bu bağlantı tarafından kullanılabilir ve bu bağlantı kapandığında kuyruk otomatik olarak silinir.
+//     false: Kuyruk diğer bağlantılar tarafından da kullanılabilir.
+// autoDelete: Kuyruğun otomatik olarak silinip silinmeyeceğini belirtir.
+
+// true: Kuyruk, onu dinleyen son abone bağlantısını kapattığında otomatik olarak silinir.
+//     false: Kuyruk, onu dinleyen son abone bağlantısını kapattığında bile silinmez.
+//     arguments: Ek kuyruk argümanları. Özel özellikler ayarlamak için kullanılabilir. Bu örnekte null olarak ayarlanmış, yani ek bir özellik yok.
+channel.QueueDeclare(queue: "hello-queue", false, false, false, arguments: null);
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);//Mesajı byte dizisine çevirir.
+
+    // Mesajı RabbitMQ kuyruğuna gönderir. Burada: exchange: Mesajın hangi exchange'e gönderileceğini belirtir. Boş string(default exchange), direkt olarak kuyruğa gönderileceğini ifade eder.
+    // routingKey: Mesajın gönderileceği kuyruk adı. basicProperties: Mesajın özelliklerini belirtir. Bu örnekte null bırakılmış.
+    // body: Gönderilecek mesajın kendisi (byte dizisi olarak).
+    channel.BasicPublish(exchange: "", routingKey: "hello-queue", basicProperties: null, body: messageBody);
+
+    Console.WriteLine($"Mesaj Gonderildi. Gönderilen mesaj: {message}");
+});
+
+Console.ReadLine();
---
+++ RabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    // Uri = new Uri("amqps://fkucsvcj:LqAqt0v5ApdauHz4EbhyT-mizaXEKoEI@cow.rmq2.cloudamqp.com/fkucsvcj")
+    HostName = "localhost"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//publisher projesnde bu kuyruk varsa bu satırı silebilirsin. Eğer kuyruk publisher da varsa onu bağlanır yoksa yeni oluşturur.
+// channel.QueueDeclare(queue: "hello-queue", false, false, false, arguments: null);
+
+channel.BasicQos(0, 10, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "hello-queue", autoAck: false, consumer: consumer);
+
+consumer.Received += (sender, e) => 
+{
+    var body = e.Body.ToArray();  
+    var message = Encoding.UTF8.GetString(body);
+    Thread.Sleep(1000);
+    Console.WriteLine($"Gelen Mesaj: {message}");
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
